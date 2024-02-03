eb20610f6f0acfb69ab531c6aca7b36962b1d598
Artyom Katsap <artyom.katsap@ride.vision>
2024-02-03 13:19:24 +0200

    added markdown preview

--- plugins.lua
+++ plugins.lua
@@ -2,78 +2,84 @@ local overrides = require "custom.configs.overrides"
 
 local plugins = {
 
-  -- Override plugin definition options
+    -- Override plugin definition options
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.configs.lspconfig"
+        end, -- Override to setup mason-lspconfig
+    },
 
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
+    -- override plugin configs
+    {
+        "williamboman/mason.nvim",
+        opts = overrides.mason,
+    },
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = overrides.treesitter,
+    },
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
+    {
+        "nvim-tree/nvim-tree.lua",
+        opts = overrides.nvimtree,
+    },
 
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
-  {
-    "kylechui/nvim-surround",
-    tag = "v2.1.4",
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup {}
-    end,
-  },
-  {
-    "nvimtools/none-ls.nvim",
+    -- Install a plugin
+    {
+        "max397574/better-escape.nvim",
+        event = "InsertEnter",
+        config = function()
+            require("better_escape").setup()
+        end,
+    },
+    {
+        "kylechui/nvim-surround",
+        tag = "v2.1.4",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup {}
+        end,
+    },
+    {
+        "nvimtools/none-ls.nvim",
 
-    event = "User FilePost",
-    config = function()
-      require "custom.configs.nonels"
-    end,
-  },
-  -- {
-  --   "stevearc/conform.nvim",
-  --   --  for users those who want auto-save conform + lazyloading!
-  --   -- event = "BufWritePre"
-  --   config = function()
-  --     require "custom.configs.conform"
-  --   end,
-  -- },
+        event = "User FilePost",
+        config = function()
+            require "custom.configs.nonels"
+        end,
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    }
+    -- {
+    --   "stevearc/conform.nvim",
+    --   --  for users those who want auto-save conform + lazyloading!
+    --   -- event = "BufWritePre"
+    --   config = function()
+    --     require "custom.configs.conform"
+    --   end,
+    -- },
 
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
+    -- To make a plugin not be loaded
+    -- {
+    --   "NvChad/nvim-colorizer.lua",
+    --   enabled = false
+    -- },
 
-  -- All NvChad plugins are lazy-loaded by default
-  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
+    -- All NvChad plugins are lazy-loaded by default
+    -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+    -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+    -- {
+    --   "mg979/vim-visual-multi",
+    --   lazy = false,
+    -- }
 }
 
 return plugins
