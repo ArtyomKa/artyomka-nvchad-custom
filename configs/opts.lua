539b0568630d5a1bc3044c4339637fe1c86a4312
Artyom Katsap <artyom.katsap@ride.vision>
2024-02-06 12:52:41 +0200

    added scrolloff

--- configs/opts.lua
+++ configs/opts.lua
@@ -2,7 +2,6 @@ local opt = vim.opt
 
 opt.colorcolumn = "80"
 opt.ignorecase = true
-
 opt.relativenumber = true
 -----------------------------------------------------------
 -- Tabs, indent
@@ -11,3 +10,5 @@ opt.expandtab = true   -- Use spaces instead of tabs
 opt.shiftwidth = 4     -- Shift 4 spaces when tab
 opt.tabstop = 4        -- 1 tab == 4 spaces
 opt.smartindent = true -- Autoindent new lines
+
+opt.scrolloff = 5
