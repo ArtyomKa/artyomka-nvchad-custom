af185dbd83c8cfad872ed67cf7fe1e835f3ef4f7
Artyom Katsap <artyom.katsap@ride.vision>
2024-02-07 13:27:46 +0200

    added vim-fugitive

--- plugins.lua
+++ plugins.lua
@@ -2,113 +2,117 @@ local overrides = require "custom.configs.overrides"
 
 local plugins = {
 
-    -- Override plugin definition options
+  -- Override plugin definition options
 
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require "plugins.configs.lspconfig"
-            require "custom.configs.lspconfig"
-        end, -- Override to setup mason-lspconfig
-    },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
 
-    -- override plugin configs
-    {
-        "williamboman/mason.nvim",
-        opts = overrides.mason,
-    },
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason,
+  },
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = overrides.treesitter,
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
 
-    {
-        "nvim-tree/nvim-tree.lua",
-        opts = overrides.nvimtree,
-    },
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
 
-    -- Install a plugin
-    {
-        "max397574/better-escape.nvim",
-        event = "InsertEnter",
-        config = function()
-            require("better_escape").setup()
-        end,
-    },
-    {
-        "kylechui/nvim-surround",
-        tag = "v2.1.4",
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup {}
-        end,
-    },
-    {
-        "nvimtools/none-ls.nvim",
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+  {
+    "kylechui/nvim-surround",
+    tag = "v2.1.4",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {}
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
 
-        event = "User FilePost",
-        config = function()
-            require "custom.configs.nonels"
-        end,
+    event = "User FilePost",
+    config = function()
+      require "custom.configs.nonels"
+    end,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+  {
+    "jakemason/ouroboros",
+    dependencies = { { "nvim-lua/plenary.nvim" } },
+    event = "User FilePost",
+    config = function()
+      vim.keymap.set("n", "<M-o>", ":Ouroboros<CR>", {})
+      -- require("ouroboros").setup({})
+    end,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    event = "User FilePost",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
     },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
     {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function()
-            vim.fn["mkdp#util#install"]()
-        end,
-    },
-    {
-        "jakemason/ouroboros",
-        dependencies = { { "nvim-lua/plenary.nvim" } },
-        event = "User FilePost",
-        config = function()
-            vim.keymap.set("n", "<M-o>", ":Ouroboros<CR>", {})
-            -- require("ouroboros").setup({})
-        end,
-    },
-    {
-        "christoomey/vim-tmux-navigator",
-        event = "User FilePost",
-        cmd = {
-            "TmuxNavigateLeft",
-            "TmuxNavigateDown",
-            "TmuxNavigateUp",
-            "TmuxNavigateRight",
-            "TmuxNavigatePrevious",
-        },
-        keys = {
-            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
-            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-        },
-    },
-    -- {
-    --   "stevearc/conform.nvim",
-    --   --  for users those who want auto-save conform + lazyloading!
-    --   -- event = "BufWritePre"
-    --   config = function()
-    --     require "custom.configs.conform"
-    --   end,
-    -- },
+        "tpope/vim-fugitive",
+        event = "VeryLazy"
+    }
+  -- {
+  --   "stevearc/conform.nvim",
+  --   --  for users those who want auto-save conform + lazyloading!
+  --   -- event = "BufWritePre"
+  --   config = function()
+  --     require "custom.configs.conform"
+  --   end,
+  -- },
 
-    -- To make a plugin not be loaded
-    -- {
-    --   "NvChad/nvim-colorizer.lua",
-    --   enabled = false
-    -- },
+  -- To make a plugin not be loaded
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   enabled = false
+  -- },
 
-    -- All NvChad plugins are lazy-loaded by default
-    -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-    -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-    -- {
-    --   "mg979/vim-visual-multi",
-    --   lazy = false,
-    -- }
+  -- All NvChad plugins are lazy-loaded by default
+  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+  -- {
+  --   "mg979/vim-visual-multi",
+  --   lazy = false,
+  -- }
 }
 
 return plugins
