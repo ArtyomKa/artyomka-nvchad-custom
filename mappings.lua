3cc141e782ecc6c28ebbc639b2946aada00524f1
Artyom Katsap <artyom.katsap@ride.vision>
2024-02-16 06:22:51 +0200

    Added debug varable value hover shortcut

--- mappings.lua
+++ mappings.lua
@@ -2,39 +2,42 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
 
-    --  format with conform
-    ["<leader>fm"] = {
-      function()
-          -- require("conform").format()
-        vim.lsp.buf.format()
-      end,
-      "formatting",
-    },
-    ["<leader>s"] = { ":%s/\\<<C-r><C-w>\\>/", "Replace word"},
-
-  },
-  v = {
-    [">"] = { ">gv", "indent"},
-  },
+		--  format with conform
+		["<leader>fm"] = {
+			function()
+				-- require("conform").format()
+				vim.lsp.buf.format()
+			end,
+			"formatting",
+		},
+		["<leader>s"] = { ":%s/\\<<C-r><C-w>\\>/", "Replace word" },
+	},
+	v = {
+		[">"] = { ">gv", "indent" },
+	},
 }
 
 -- more keybinds!
 M.dap = {
-    n = {
-        ["<F9>"] = {"<cmd> DapToggleBreakpoint <CR>"},
-        ["<C-F5>"] = {
-            function() 
-                require('dap').continue()
-            end
-        },
-        ["<F10>"] = {"<cmd> DapStepOver <CR>"},
-        ["<F11>"] = {"<cmd> DapStepInto <CR>"},
-        ["<S-F11>"] = {"<cmd> DapStepOut <CR>"},
-        ["<F5>"] = {"<cmd> DapContinue <CR>"},
-
-    }
+	n = {
+		["<F9>"] = { "<cmd> DapToggleBreakpoint <CR>" },
+		["<C-F5>"] = {
+			function()
+				require("dap").continue()
+			end,
+		},
+		["<F10>"] = { "<cmd> DapStepOver <CR>" },
+		["<F11>"] = { "<cmd> DapStepInto <CR>" },
+		["<S-F11>"] = { "<cmd> DapStepOut <CR>" },
+		["<F5>"] = { "<cmd> DapContinue <CR>" },
+		["<M-k>"] = {
+			function()
+				require("dapui").eval()
+			end,
+		},
+	},
 }
 return M
