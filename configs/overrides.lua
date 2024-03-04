7b91588ec1b86df285be1848b340a792b4647a65
Artyom Katsap <artyom.katsap@ride.vision>
2024-03-04 09:05:23 +0200

    added vimdoc to treesitter

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -1,75 +1,74 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "lua",
-    "html",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "c",
-    "cmake",
-    "markdown",
-    "markdown_inline",
-    "dockerfile",
-  },
-  indent = {
-    enable = true,
-    -- disable = {
-    --   "python"
-    -- },
-  },
+    ensure_installed = {
+        "vim",
+        "vimdoc",
+        "lua",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "c",
+        "cmake",
+        "markdown",
+        "markdown_inline",
+        "dockerfile",
+    },
+    indent = {
+        enable = true,
+        -- disable = {
+        --   "python"
+        -- },
+    },
 }
 
 M.mason = {
-  ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
+    ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
 
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "deno",
-    "prettier",
+        -- web dev stuff
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "deno",
+        "prettier",
 
-    -- c/cpp stuff
-    "clangd",
-    "clang-format",
-    "cmake-language-server",
-    --python
-    "pyright",
-    "black",
-    "ruff",
-    --docker
-    "dockerfile-language-server",
-    "docker-compose-language-service",
-    "hadolint",
-    --debugging
-    "debugpy",
-    "codelldb"
-
-
-  },
+        -- c/cpp stuff
+        "clangd",
+        "clang-format",
+        "cmake-language-server",
+        --python
+        "pyright",
+        "black",
+        "ruff",
+        --docker
+        "dockerfile-language-server",
+        "docker-compose-language-service",
+        "hadolint",
+        --debugging
+        "debugpy",
+        "codelldb",
+    },
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-  git = {
-    enable = true,
-  },
-
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
+    git = {
+        enable = true,
     },
-  },
+
+    renderer = {
+        highlight_git = true,
+        icons = {
+            show = {
+                git = true,
+            },
+        },
+    },
 }
 
 return M
